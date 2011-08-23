c81c196225dae9ee3709386e51d2ed8518241d45
Rob Williams <rob.williams@ontometrics.com>
2011-08-23 11:39:17 -0700

    Adding ability to extract a tag that matches a string.

--- src/main/java/com/ontometrics/scraper/TagOccurrence.java
+++ src/main/java/com/ontometrics/scraper/TagOccurrence.java
@@ -1,23 +1,36 @@
 package com.ontometrics.scraper;
 
 public class TagOccurrence {
-	
+
 	private String tag;
-	
+
 	private int occurrence;
-	
+
+	private String matching;
+
 	public TagOccurrence(String tag, int occurrence) {
 		super();
 		this.tag = tag;
 		this.occurrence = occurrence;
 	}
 
+	public TagOccurrence(String tag, int occurrence, String matching) {
+		super();
+		this.tag = tag;
+		this.occurrence = occurrence;
+		this.matching = matching;
+	}
+
 	public int getOccurrence() {
 		return occurrence;
 	}
-	
+
 	public String getTag() {
 		return tag;
+	}
+
+	public String getMatching() {
+		return matching;
 	}
 
 }
