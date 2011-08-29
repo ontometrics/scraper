bf4e90b23763f08bceffab3e9971765d7aeadae4
Rob Williams <rob.williams@ontometrics.com>
2011-08-29 06:42:46 -0700

    Fixing hideous html errors..

---
+++ src/main/java/com/ontometrics/scraper/grants/GrantsNetScraper.java
@@ -0,0 +1,5 @@
+package com.ontometrics.scraper.grants;
+
+public class GrantsNetScraper {
+
+}
---
+++ src/test/java/com/ontometrics/scraper/grants/GrantsNetScraperTest.java
@@ -0,0 +1,12 @@
+package com.ontometrics.scraper.grants;
+
+import org.junit.Test;
+
+public class GrantsNetScraperTest {
+
+	@Test
+	public void test() {
+
+	}
+
+}
