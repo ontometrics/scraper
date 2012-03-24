0c16ada795be108d6b243cde63edfd1b3dfad684
Garam Song <garam.song@ontometrics.com>
2012-03-23 19:33:00 -0700

    Add name to link

--- src/main/java/com/ontometrics/scraper/extraction/Link.java
+++ src/main/java/com/ontometrics/scraper/extraction/Link.java
@@ -5,11 +5,45 @@ public class Link implements Field {
 	private String label;
 
 	private String href;
+	
+	private String name;
 
+	public static class Builder {
+		private String label;
+		private String href;
+		private String name;
+
+		public Builder label(String label) {
+			this.label = label;
+			return this;
+		}
+		
+		public Builder href(String href) {
+			this.href = href;
+			return this;
+		}
+		
+		public Builder name(String name) {
+			this.name = name;
+			return this;
+		}
+		
+		public Link build() {
+			return new Link(this);
+		}
+	}
+	
 	public Link(String label, String href) {
 		super();
 		this.label = label;
 		this.href = href;
+	}
+
+	public Link(Builder builder) {
+		super();
+		this.label = builder.label;
+		this.href = builder.href;
+		this.name = builder.name;
 	}
 
 	public String getLabel() {
@@ -18,6 +52,10 @@ public class Link implements Field {
 
 	public String getHref() {
 		return href;
+	}
+	
+	public String getName() {
+		return name;
 	}
 
 	@Override
