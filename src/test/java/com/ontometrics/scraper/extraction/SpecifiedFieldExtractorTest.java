e76840a16aed37279f0f9f03ee1ac6629549dd9a
Rob Williams <rob@ontometrics.com>
2013-07-09 11:03:04 -0700

    Testing Specific Fields
    Pulled fields out into a map in the test and then asserting on the
    contents of the various fields.

--- src/test/java/com/ontometrics/scraper/extraction/SpecifiedFieldExtractorTest.java
+++ src/test/java/com/ontometrics/scraper/extraction/SpecifiedFieldExtractorTest.java
@@ -6,8 +6,10 @@ import static org.hamcrest.CoreMatchers.is;
 import static org.junit.Assert.assertThat;
 import static org.junit.matchers.JUnitMatchers.containsString;
 
-import java.util.ArrayList;
+import java.util.Arrays;
+import java.util.HashMap;
 import java.util.List;
+import java.util.Map;
 
 import org.junit.Before;
 import org.junit.Test;
@@ -20,13 +22,14 @@ public class SpecifiedFieldExtractorTest {
 
 	private SpecifiedFieldExtractor extractor;
 
+	private Map<String, Field> fieldsFound = new HashMap<String, Field>();
+
 	@Before
 	public void setup() {
-		List<String> fields = new ArrayList<String>();
-		fields.add("job_title");
-		fields.add("job_desc");
-		fields.add("job_req");
-		this.extractor = new SpecifiedFieldExtractor.Builder().identifyFieldsByClass().addFields(fields).build();
+		this.extractor = new SpecifiedFieldExtractor.Builder()
+				.identifyFieldsByClass()
+					.addFields(Arrays.asList("job_title", "job_desc", "job_req"))
+					.build();
 	}
 
 	@Test
@@ -36,15 +39,10 @@ public class SpecifiedFieldExtractorTest {
 		assertThat(extractor.getFieldsToGet().size(), is(1));
 
 	}
-	
+
 	@Test
-	public void canBuildUsingListOfFields(){
-		List<String> fields = new ArrayList<String>();
-		fields.add("job_title");
-		fields.add("job_desc");
-		fields.add("job_req");
-		this.extractor = new SpecifiedFieldExtractor.Builder().identifyFieldsByClass().addFields(fields).build();
-		
+	public void canBuildUsingListOfFields() {
+
 		assertThat(extractor.getFieldsToGet().size(), is(3));
 	}
 
@@ -53,9 +51,19 @@ public class SpecifiedFieldExtractorTest {
 		List<Field> fields = extractor.source(html().url(CareerBuilderDetailPage.getUrl())).getFields();
 		log.info("fields found: {}", fields);
 
-		Field description = fields.get(0);
+		extractFoundFields(fields);
 
+		Field title = fieldsFound.get("job_title");
+		Field description = fieldsFound.get("job_desc");
+
+		assertThat(title.getValue(), is("Front End / HTML Developer"));
 		assertThat(description.getValue(), containsString("Front End / HTML Developer"));
+	}
+
+	private void extractFoundFields(List<Field> fields) {
+		for (Field field : fields) {
+			fieldsFound.put(field.getLabel(), field);
+		}
 	}
 
 }
