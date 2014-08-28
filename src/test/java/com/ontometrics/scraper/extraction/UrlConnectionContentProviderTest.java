699131f1f00bffbac94c805b9a64d07ac7d31936
Andrey Chorniy <andrey.chorniy@gmail.com>
2014-08-28 13:50:23 +0300

    Added tests for UrlConnectionContentProvider.java to check it can access resources which require User-Agent request header - so, test that request-property has been passed

--- src/test/java/com/ontometrics/scraper/extraction/UrlConnectionContentProviderTest.java
+++ src/test/java/com/ontometrics/scraper/extraction/UrlConnectionContentProviderTest.java
@@ -2,10 +2,12 @@ package com.ontometrics.scraper.extraction;
 
 import com.ontometrics.scraper.TestUtil;
 import org.apache.commons.io.IOUtils;
-import org.hamcrest.CoreMatchers;
 import org.hamcrest.Matchers;
 import org.junit.Assert;
+import org.junit.Before;
 import org.junit.Test;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
 
 import java.io.IOException;
 import java.io.InputStream;
@@ -22,23 +24,35 @@ import static org.hamcrest.MatcherAssert.assertThat;
   */
 public class UrlConnectionContentProviderTest {
 
+    private static final Logger logger = LoggerFactory.getLogger(UrlConnectionContentProviderTest.class);
+
+    private UrlConnectionContentProvider provider;
+
+    @Before
+    public void init(){
+        provider = new UrlConnectionContentProvider.Builder().build();
+    }
+
     @Test
     public void canReadDataFromLocalFileUrl() {
-        UrlConnectionContentProvider provider = new UrlConnectionContentProvider();
         checkUrlAccessible(TestUtil.getFileAsURL("/testpages/ids-page-2.html"), provider);
     }
 
     @Test
     public void canReadDataFromLocalFileUrlWithRequestAttributesSet() {
-        UrlConnectionContentProvider provider = new UrlConnectionContentProvider();
-//        provider.addRequestAttribute("User-Agent", "Mozilla/5.0");
+        provider = new UrlConnectionContentProvider.Builder().setRequestProperty("User-Agent", "Mozilla/5.0").build();
         checkUrlAccessible(TestUtil.getFileAsURL("/testpages/ids-page-2.html"), provider);
     }
 
     @Test
     public void canReadDataFromExternalUrl() throws MalformedURLException {
-        UrlConnectionContentProvider provider = new UrlConnectionContentProvider();
         checkUrlAccessible(new URL("http://google.com"), provider);
+    }
+
+    @Test
+    public void canReadDataFromExternalUrlWhichRequireUserAgent() throws MalformedURLException {
+        provider = new UrlConnectionContentProvider.Builder().setRequestProperty("User-Agent", "Mozilla/5.0").build();
+        checkUrlAccessible(new URL("http://www.glassdoor.com/Job/california-java-developer-job-opportunities-SRCH_IL.0,10_IS2280_KO11,25_IP6.htm"), provider);
     }
 
 
@@ -52,6 +66,7 @@ public class UrlConnectionContentProviderTest {
             assertThat(IOUtils.read(is, byteArray), is(64));
 
         } catch (IOException ex) {
+            logger.error("Failed to load resource from url "+url, ex);
             Assert.fail("Failed to load resource from url "+url);
         } finally {
             IOUtils.closeQuietly(is);
